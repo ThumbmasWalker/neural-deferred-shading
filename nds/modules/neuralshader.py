1980da689f8e0d11f1fee68758cfd8448bf7969d
Tom Walker <58395984+ThumbmasWalker@users.noreply.github.com>
2023-01-13 14:35:38 +0000

    Add files via upload

--- nds/modules/neuralshader.py
+++ nds/modules/neuralshader.py
@@ -81,3 +81,75 @@ class NeuralShader(torch.nn.Module):
         if isinstance(self.fourier_feature_transform, GaussianFourierFeatureTransform):
             data['B'] = self.fourier_feature_transform.B
         torch.save(data, path)
+
+class NeuralExplicit(torch.nn.Module):
+
+    def __init__(self,
+                 hidden_features_size=256,
+                 hidden_features_layers=7,
+                 activation='relu',
+                 last_activation=None,
+                 fourier_features='none',
+                 mapping_size=256,
+                 fft_scale=10,
+                 device='cpu'):
+
+        super().__init__()
+        self.fourier_feature_transform = None
+        
+        if fourier_features == 'None':
+     
+            self.mlp = FC(2, 3, [hidden_features_size] * hidden_features_layers, activation, None).to(device)
+            
+        
+        
+        # Store the config
+        
+        self._config = {
+            'hidden_features_size': hidden_features_size,
+            'hidden_features_layers': hidden_features_layers,
+            'activation': activation,
+            'last_activation': last_activation,
+            'fourier_features': fourier_features,
+            'mapping_size': mapping_size,
+            'fft_scale': fft_scale
+        }
+
+    def forward(self, coords):
+        #! The current apporach only transforms the positions
+        #! However it is also possible to transform all the input (views and normals)
+        #if self.fourier_feature_transform is not None:
+        #    h = self.diffuse(self.fourier_feature_transform(position))
+        #    h = torch.cat([h, normal, view_dir], dim=-1)
+        #else:
+        #    h = self.diffuse(position)
+        #    h = torch.cat([h, normal, view_dir], dim=-1)
+
+        return self.mlp(coords)
+
+    @classmethod
+    def load(cls, path, device='cpu'):
+        data = torch.load(path, map_location=device)
+
+        # Convert data between versions
+        version = data['version']
+
+        shader = cls(**data['config'], device=device)
+        shader.load_state_dict(data['state_dict'])
+
+        if version < 2 and isinstance(shader.fourier_feature_transform, GaussianFourierFeatureTransform):
+            print("Warning: B matrix for GFFT features is not stored in checkpoints of versions < 2")
+        elif isinstance(shader.fourier_feature_transform, GaussianFourierFeatureTransform):
+            shader.fourier_feature_transform.B = data['B']
+        return shader
+
+    def save(self, path):
+        data = {
+            'version': 2,
+            'config': self._config,
+            'state_dict': self.state_dict()
+        }
+
+        if isinstance(self.fourier_feature_transform, GaussianFourierFeatureTransform):
+            data['B'] = self.fourier_feature_transform.B
+        torch.save(data, path)
